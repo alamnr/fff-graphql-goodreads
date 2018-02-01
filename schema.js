b2ce7f7055deed5e7569c717e3b0c26c2264b915
nalam <alamnr@gmail.com>
2018-02-01 14:41:29 +0600

    lazy fetching example

--- schema.js
+++ schema.js
@@ -28,11 +28,13 @@ const BookType = new GraphQLObjectType({
     fields: () => ({
         title:{
             type: GraphQLString,
-            resolve: xml=>xml.title[0]
+            //resolve: xml=>xml.title[0]
+            resolve: xml=>xml.GoodreadsResponse.book[0].title[0]
         },
         isbn:{
             type: GraphQLString,
-            resolve: xml => xml.isbn[0]
+            //resolve: xml => xml.isbn[0]
+            resolve: xml => xml.GoodreadsResponse.book[0].isbn[0]
         }
     })
 })
@@ -50,7 +52,15 @@ const AuthorType = new GraphQLObjectType({
         },
         books:{
             type: new GraphQLList(BookType),
-            resolve: xml => xml.GoodreadsResponse.author[0].books[0].book
+            //resolve: xml => xml.GoodreadsResponse.author[0].books[0].book
+            resolve:xml=> { 
+                const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem=> elem.id[0]._)
+                console.log('Fetching Books...'+ ids)
+                return Promise.all(ids.map(id=>
+                fetch(`https://www.goodreads.com/book/show/${id}.xml?key=92WHmqw4RL2nx76JiypQ`)
+                .then(response => response.text())
+                .then(parseXml)))
+            }
         }
     })
 })
